39240683afd815c9d8f91d058837eb915f13c953
gagan86nagpal <gagannagpal68@gmail.com>
2018-03-05 09:38:57 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,11 +14,22 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
     console.log('New Client connected');
-    
+    // Info to everyone except the user who  joined
+    socket.broadcast.emit('newMessage',{
+        from:"admin",
+        text:"New User Joined",
+        createdAt:new Date().getTime()
+    })
+    // Greeting to user just joined by socket.emit
+    socket.emit('newMessage',{
+        from:"admin",
+        text:"Welcome",
+        createdAt:new Date().getTime()
+    })
     socket.on('disconnect',()=>{
         console.log('Client Disconnected');
     })
-    
+    // If a client sends a message , it is resent to everyone including sender
     socket.on('createMessage',(msg)=>{
         io.emit('newMessage',{
             from:msg.from,
